ee2674fa22beeda8f1283afb1977710ad133b8c7
Victor Wibisono <victor.wibisono@artefact.com>
2022-05-07 15:56:17 +0800

    changed startup to install script

--- install.sh
+++ install.sh
@@ -44,22 +44,45 @@ do
   fi
 done
 
-cd ~
+cd /home/demouser
 
-export VERSION=10.0.2
-export DATA_DIR=
-export PORT=
-
-# Check if DSS directory exists
-if [ -d "~/" ]
-then
-    echo "Directory /path/to/dir exists."
-else
-    echo "Error: Directory /path/to/dir does not exists."
-fi
+export VERSION=10.0.5
+export INSTALLER_NAME=dataiku-dss-$VERSION
+export DATA_DIR=$INSTALLER_NAME-data
+export PORT=10000
 
 # Check if DATA_DIR directory exists
+if [ -d "$DATA_DIR" ]
+then
+    echo "Directory $DATA_DIR exists. Removing."
+    rm -r $DATA_DIR
+else
+    echo "Directory $DATA_DIR does not exists."
+fi
 
-wget https://cdn.downloads.dataiku.com/public/dss/$VERSION/dataiku-dss-$VERSION.tar.gz
+# Check if DSS directory exists
+if [ -d "$INSTALLER_NAME" ]
+then
+    echo "Directory $INSTALLER_NAME exists. Removing."
+    rm -r $INSTALLER_NAME
+else
+    echo "Directory $INSTALLER_NAME does not exists."
+fi
 
-tar xzf dataiku-dss-$VERSION.tar.gz
+# Check if DSS installer file exists
+if [ -f "$INSTALLER_NAME.tar.gz" ]
+then
+    echo "File $INSTALLER_NAME.tar.gz exists. Removing."
+    rm -r $INSTALLER_NAME.tar.gz
+else
+    echo "Directory $INSTALLER_NAME does not exists."
+fi
+
+wget https://cdn.downloads.dataiku.com/public/dss/$VERSION/$INSTALLER_NAME.tar.gz
+
+tar xzf $INSTALLER_NAME.tar.gz
+
+# Install Dataiku DSS
+$INSTALLER_NAME/installer.sh -y -d $DATA_DIR -p $PORT -l license.json
+
+$DATA_DIR/bin/start
